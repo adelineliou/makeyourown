64f0aaaacbab3637b1f07a9844a7dcc66832feb3
adelineliou <adeline.liou@gmail.com>
2019-08-23 15:24:58 -0700

    testing

---
+++ main.rkt
@@ -0,0 +1,6 @@
+#lang racket/base
+
+(provide (all-from-out "./housemaker/house.rkt"
+                       "./facemaker/face.rkt"))
+(require "./housemaker/house.rkt"
+                       "./facemaker/face.rkt")
